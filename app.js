876b9cf7e1e077f60f0aac2db15f4111334d2d93
Joshhortt <joshhortt@yahoo.com>
2022-06-18 18:10:40 +0100

    moved images , styles into express static public

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
 app.listen(3000, function()  {
     console.log("Server is running on port 3000");
-})
+});
