7c246e85765dbf85f42abbb6065d0670cee50bb5
Joshhortt <joshhortt@yahoo.com>
2022-06-18 18:26:20 +0100

    signed bodyParser, add action POST method to form

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function (req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    console.log(firstName, lastName, email);
+});
+
+
 
 app.listen(3000, function()  {
     console.log("Server is running on port 3000");
